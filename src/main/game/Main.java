27440c41e1d792d66a51a70f8790390d94a060b6
Mabuu <phamducthang03102002@gmail.com>
2021-12-02 19:48:13 +0700

    make clean main class

--- src/main/game/Main.java
+++ src/main/game/Main.java
@@ -1,43 +1,19 @@
 package main.game;
 
-import main.engine.Window;
-import org.lwjgl.glfw.GLFW;
+import main.engine.GameEngine;
+import main.engine.GameLogic;
+import main.engine.objects.BommanGame;
 
-public class Main implements Runnable{
-  public Thread game;
-  public Window window;
-  public static final int width = 1280;
-  public static final int height = 760;
-
-  public void start() {
-    game = new Thread(this, "game");
-    game.start();
-  }
-
-  public void init() {
-    window = new Window(width, height, "TEST");
-    window.create();
-  }
-
-  @Override
-  public void run() {
-    init();
-
-    // ESC button
-    while (!window.isClose()) {
-      update();
-      render();
+public class Main {
+ 
+    public static void main(String[] args) {
+        try {
+            GameLogic gameLogic = new BommanGame();
+            GameEngine gameEng = new GameEngine("GAME", 600, 480, gameLogic);
+            gameEng.run();
+        } catch (Exception excp) {
+            excp.printStackTrace();
+            System.exit(-1);
+        }
     }
-    window.destroy();
-  }
-
-  private void update() {
-    window.update();
-  }
-  private void render() { window.render();}
-
-
-  public static void main(String[] args) {
-    new Main().start();
-  }
 }
