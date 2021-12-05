97d513d68b1b8568126a53db86165130aa73a08f
Jo3yVX <anhquyethgvx@gmail.com>
2021-12-06 06:23:11 +0700

    update graphic classses and fix conflicts

--- src/main/engine/util/graphic/Texture.java
+++ src/main/engine/util/graphic/Texture.java
@@ -1,148 +1,116 @@
 package main.engine.util.graphic;
 
-import static org.lwjgl.opengl.GL11.glPixelStorei;
-import static org.lwjgl.opengl.GL33.GL_CLAMP_TO_EDGE;
-import static org.lwjgl.opengl.GL33.GL_DEPTH_COMPONENT;
-import static org.lwjgl.opengl.GL33.GL_FLOAT;
-import static org.lwjgl.opengl.GL33.GL_NEAREST;
-import static org.lwjgl.opengl.GL33.GL_RGBA;
-import static org.lwjgl.opengl.GL33.GL_TEXTURE_2D;
-import static org.lwjgl.opengl.GL33.GL_TEXTURE_MAG_FILTER;
-import static org.lwjgl.opengl.GL33.GL_TEXTURE_MIN_FILTER;
-import static org.lwjgl.opengl.GL33.GL_TEXTURE_WRAP_S;
-import static org.lwjgl.opengl.GL33.GL_TEXTURE_WRAP_T;
-import static org.lwjgl.opengl.GL33.GL_UNPACK_ALIGNMENT;
-import static org.lwjgl.opengl.GL33.GL_UNSIGNED_BYTE;
-import static org.lwjgl.opengl.GL33.glBindTexture;
-import static org.lwjgl.opengl.GL33.glGenTextures;
-import static org.lwjgl.opengl.GL33.glGenerateMipmap;
-import static org.lwjgl.opengl.GL33.glTexImage2D;
-import static org.lwjgl.opengl.GL33.glTexParameteri;
-import static org.lwjgl.stb.STBImage.stbi_failure_reason;
-import static org.lwjgl.stb.STBImage.stbi_load;
-import static org.lwjgl.stb.STBImage.stbi_set_flip_vertically_on_load;
+import java.nio.IntBuffer;
 
 import java.nio.ByteBuffer;
-import java.nio.IntBuffer;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL12.*;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.stb.STBImage.*;
 import org.lwjgl.system.MemoryStack;
+import static org.lwjgl.system.MemoryStack.*;
+import main.engine.util.io.FileUtils;
 
-/** Represent a texture. Could use slick-utils-3 Texture instead of this class. */
 public class Texture {
 
   private final int id;
-  private int width;
-  private int height;
-  private int pixelFormat;
+
+  private final int width;
+
+  private final int height;
+
+  private int numRows = 1;
+
+  private int numCols = 1;
+
+  private String path;
 
   /**
-   * Create empty texture.
+   * Creates an empty texture.
    *
-   * @param width width of tx
-   * @param height height of tx
-   * @param pixelFormat format of pixel data/color (rgba, rgb, etc)
+   * @param width Width of the texture
+   * @param height Height of the texture
+   * @param pixelFormat Specifies the format of the pixel data (GL_RGBA, etc.)
    * @throws Exception
    */
   public Texture(int width, int height, int pixelFormat) throws Exception {
     this.id = glGenTextures();
     this.width = width;
     this.height = height;
-    this.pixelFormat = pixelFormat;
+    glBindTexture(GL_TEXTURE_2D, this.id);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, this.width, this.height, 0, pixelFormat, GL_FLOAT, (ByteBuffer) null);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   }
 
-  public Texture() {
-    this.id = glGenTextures();
+  public Texture(String fileName, int numCols, int numRows) throws Exception {
+    this(fileName);
+    this.numCols = numCols;
+    this.numRows = numRows;
   }
 
-  public static Texture createTexture(int width, int height, ByteBuffer data) {
-    Texture texture = new Texture();
-    texture.setWidth(width);
-    texture.setHeight(height);
-    texture.bind();
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    // Upload texture data
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-    // Generate mip map
-    glGenerateMipmap(GL_TEXTURE_2D);
-    return texture;
+  public Texture(String fileName) throws Exception {
+    this(FileUtils.ioResourceToByteBuffer(fileName, 1024));
   }
 
-  public static Texture loadTexture(String path) {
-    ByteBuffer image;
-    int width, height;
-    try (MemoryStack stack = MemoryStack.stackPush()) {
-      /* Prepare image buffers */
+  public Texture(ByteBuffer imageData) {
+    try (MemoryStack stack = stackPush()) {
       IntBuffer w = stack.mallocInt(1);
       IntBuffer h = stack.mallocInt(1);
-      IntBuffer comp = stack.mallocInt(1);
+      IntBuffer avChannels = stack.mallocInt(1);
 
-      /* Load image */
-      stbi_set_flip_vertically_on_load(true);
-      image = stbi_load(path, w, h, comp, 4);
-      if (image == null) {
-        throw new RuntimeException(
-            "Failed to load a texture file!" + System.lineSeparator() + stbi_failure_reason());
-      }
+      // Decode texture image into a byte buffer
+      ByteBuffer decodedImage = stbi_load_from_memory(imageData, w, h, avChannels, 4);
 
-      /* Get width and height of image */
-      width = w.get();
-      height = h.get();
+      this.width = w.get();
+      this.height = h.get();
+
+      // Create a new OpenGL texture
+      this.id = glGenTextures();
+      // Bind the texture
+      glBindTexture(GL_TEXTURE_2D, this.id);
+
+      // Tell OpenGL how to unpack the RGBA bytes. Each component is 1 byte size
+      glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+      // Upload the texture data
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this.width, this.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, decodedImage);
+      // Generate Mip Map
+      glGenerateMipmap(GL_TEXTURE_2D);
+
+      stbi_image_free(decodedImage);
     }
-    return createTexture(width, height, image);
   }
 
-  private void bind() {
-    glBindTexture(GL_TEXTURE_2D, this.id);
+  public int getNumCols() {
+    return numCols;
+  }
+
+  public int getNumRows() {
+    return numRows;
+  }
+
+  public int getWidth() {
+    return this.width;
+  }
+
+  public int getHeight() {
+    return this.height;
+  }
+
+  public void bind() {
+    glBindTexture(GL_TEXTURE_2D, id);
   }
 
   public int getId() {
     return id;
   }
 
-  public int getWidth() {
-    return width;
-  }
-
-  public void setWidth(int width) {
-    this.width = width;
-  }
-
-  public int getHeight() {
-    return height;
-  }
-
-  public void setHeight(int height) {
-    this.height = height;
-  }
-
-  public int getPixelFormat() {
-    return pixelFormat;
-  }
-
-  public void setPixelFormat(int pixelFormat) {
-    this.pixelFormat = pixelFormat;
-  }
-
-  private void initSettings() {
-    glBindTexture(GL_TEXTURE_2D, this.id);
-    glTexImage2D(
-        GL_TEXTURE_2D,
-        0,
-        GL_DEPTH_COMPONENT,
-        this.width,
-        this.height,
-        0,
-        pixelFormat,
-        GL_FLOAT,
-        (ByteBuffer) null);
-    glTexParameteri(
-        GL_TEXTURE_2D,
-        GL_TEXTURE_MIN_FILTER,
-        GL_NEAREST); // may change to linear for better performance ?
-    glTexParameteri(
-        GL_TEXTURE_2D,
-        GL_TEXTURE_MAG_FILTER,
-        GL_NEAREST); // may change to linear for better performance ?
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  public void cleanup() {
+    glDeleteTextures(id);
   }
 }
